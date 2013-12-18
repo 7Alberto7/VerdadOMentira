a696b11f78806376f2bbfeee5a1107b51541caf2
7Alberto7 <j.albertocg86@gmail.com>
2013-12-18 16:06:37 +0100

    Realizados pequeños arreglos

--- gen/com/example/verdadomentira/R.java
+++ gen/com/example/verdadomentira/R.java
@@ -73,18 +73,17 @@ public final class R {
         public static final int verdad3=0x7f040005;
     }
     public static final class string {
-        public static final int action_settings=0x7f070007;
         public static final int app_name=0x7f070000;
         public static final int habla=0x7f070004;
-        public static final int hello_world=0x7f070008;
         public static final int jugar=0x7f070005;
         public static final int nosotros=0x7f070002;
-        public static final int nosotros_texto=0x7f07000a;
+        public static final int nosotros_texto=0x7f070009;
+        public static final int pulsar_para_hablar=0x7f070006;
         public static final int title_activity_juego=0x7f070003;
-        public static final int title_activity_nosotros=0x7f070009;
-        public static final int title_activity_tutorial=0x7f070006;
+        public static final int title_activity_nosotros=0x7f070008;
+        public static final int title_activity_tutorial=0x7f070007;
         public static final int tutorial=0x7f070001;
-        public static final int tutorial_texto=0x7f07000b;
+        public static final int tutorial_texto=0x7f07000a;
     }
     public static final class style {
         /** 
